8a331dd4d5144e6e6f51ad01d596bee2a377a8dc
dpathim <dpdevops6@gmail.com>
2023-09-25 14:54:28 -0400

    Terraform

---
+++ vars.tf
@@ -0,0 +1,16 @@
+variable "env" {}
+variable "tags" {}
+variable "subnet_ids" {}
+variable "vpc_id" {}
+variable "sg_ingress_cidr" {}
+variable "elesticache_type" {}
+variable "family" {}
+variable "port" {}
+variable "engine" {}
+variable "node_type" {}
+variable "num_cache_nodes" {}
+variable "engine_version" {}
+
+
+
+
